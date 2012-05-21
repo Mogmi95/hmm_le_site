79d032d3b52381fb8d1436e6886f8d93ca74a409
Mogmi <mogmi95@gmail.com>
2012-05-21 14:56:31 +0200

    Updated the database modelisation

--- strip/models.py
+++ strip/models.py
@@ -1,12 +1,22 @@
 from django.db import models
 
 class Tag(models.Model):
+    """
+    This class describes a Tag, which is a keyword used to give information
+    about the strip who's linked to it.
+    A Tag can describes more than one Strip, but a Strip cannot contains
+    multiple links to the same Tag.
+    """
     def __unicode__(self):
-        return tag_name
+        return self.tag_name
     # The name of the tag
     tag_name = models.CharField(max_length=50)
 
 class Strip(models.Model):
+    """
+    This class describes a Strip, which represents every information concerning
+    a strip (its number, its comments, etc).
+    """
     def __unicode__(self):
         return str(self.number) + " - " + self.fr_title
 
@@ -22,33 +32,37 @@ class Strip(models.Model):
     en_title = models.CharField(max_length=50)
 
     # The french description of the strip
-    fr_desc = models.CharField(max_length=250)
+    fr_description = models.CharField(max_length=250)
     # The english description of the strip
-    en_desc = models.CharField(max_length=250)
+    en_description = models.CharField(max_length=250)
 
     # The PNG file for the french version
-    png_file_fr = models.ImageField(upload_to="png_fr/")
+    png_fr = models.ImageField(upload_to="png_fr/")
     # The PNG file for the english version
-    png_file_en = models.ImageField(upload_to="png_en/")
+    png_en = models.ImageField(upload_to="png_en/")
 
     # The SVG file for the french version
-    svg_file_fr = models.FileField(upload_to="svg_fr/")
+    svg_fr = models.FileField(upload_to="svg_fr/")
     # The SVG file for the english version
-    svg_file_en = models.FileField(upload_to="svg_en/")
+    svg_en = models.FileField(upload_to="svg_en/")
 
     # The tags linked to this Strip
     tags = models.ManyToManyField(Tag)
 
 class Comment(models.Model):
+    """
+    This class describes a comment, which is a message posted by a user on a
+    Strip.
+    """
     def __unicode__(self):
-        return "[" + str(self.related_strip.number) + "] " +\
-            self.author_name + " (" + str(self.date) + ") : " +\
+        return self.author_name + " (" + str(self.date) + ") : " +\
             self.comment
 
     # The author's name
     author_name = models.CharField(max_length=50)
+
     # The author's website
-    author_website = models.CharField(max_length=50)
+    author_website = models.CharField(max_length=50, blank=True)
 
     # The comment
     comment = models.CharField(max_length=250)
@@ -57,7 +71,7 @@ class Comment(models.Model):
     date = models.DateField()
 
     # Strip where the comment was posted
-    related_strip = models.ForeignKey(Strip)
+    strip = models.ForeignKey(Strip)
 
     # Is validated by the admin
     validated = models.BooleanField()
@@ -66,4 +80,3 @@ class Comment(models.Model):
     class Admin:
         list_display = ('date', 'author_name', 'validated')
         search_field = ('author_name')
-
