12e84497c62a8bd4ad5a7480256b46dd8453a5e0
Mogmi <mogmi95@gmail.com>
2012-05-28 21:39:50 +0200

    Added handler for the strips' comments

--- strip/handlers.py
+++ strip/handlers.py
@@ -2,13 +2,33 @@ from piston.handler import BaseHandler
 from piston.utils import rc
 from strip.models import Strip
 from strip.models import Tag
+from strip.models import Comment
 
 class StripHandler(BaseHandler):
     model = Strip
     allowed_methods = ('GET',)
-    exclude = ('id',)
+    fields = (
+        "number",
+        "date",
+        "fr_title",
+        "en_title",
+        "fr_description",
+        "en_description",
+        "png_fr", ('url'),
+        "png_en",
+        "svg_fr",
+        "svg_en",
+        "tags",
+    )
 
     def read(self, request, number=None):
+        """
+        Request from the API for strips :
+            - if number is not given, we return all the strips
+            - if it is, we return the strip with the corresponding
+            number
+            - if the strip doesn't exist, we return a 404 error
+        """
         base = Strip.objects
         if (number):
             if ((int(number) == 0) or (int(number) > len(base.all()))):
@@ -20,6 +40,33 @@ class StripHandler(BaseHandler):
             return base.all()
 
 class TagHandler(BaseHandler):
+    """
+    This handler is only here to delete the 'id' value
+    from tags in the API results
+    """
     model = Tag
     allowed_methods = ('GET',)
     exclude = ('id',)
+
+class StripCommentHandler(BaseHandler):
+    model = Comment
+    allowed_methods = ('GET',)
+    fields = (
+       'author_name', 
+       'author_website', 
+       'comment', 
+       'date', 
+    )
+
+    def read(self, request, number):
+        """
+        This method is used to get comments on a strip indicated by
+        'number'
+        """
+        base = Strip.objects
+        if ((int(number) == 0) or (int(number) > len(base.all()))):
+            resp = rc.NOT_FOUND
+            return resp
+        else:
+            return base.get(number=number).comment_set.all()
+
--- strip/urls.py
+++ strip/urls.py
@@ -1,6 +1,7 @@
 from django.conf.urls.defaults import *
 from piston.resource import Resource
 from strip.handlers import StripHandler
+from strip.handlers import StripCommentHandler
 
 class CsrfExemptResource( Resource ):
     def __init__( self, handler, authentication = None ):
@@ -8,8 +9,10 @@ class CsrfExemptResource( Resource ):
         self.csrf_exempt = getattr( self.handler, 'csrf_exempt', True )
 
 strip_resource = CsrfExemptResource(StripHandler)
+strip_comment_resource = CsrfExemptResource(StripCommentHandler)
 
 urlpatterns = patterns( '',
-    url( r'^(?P<emitter_format>.+)/strip/(?P<number>\d+)$', strip_resource),
     url( r'^(?P<emitter_format>.+)/strips$', strip_resource),
+    url( r'^(?P<emitter_format>.+)/strip/(?P<number>\d+)$', strip_resource),
+    url( r'^(?P<emitter_format>.+)/strip/comments/(?P<number>\d+)$', strip_comment_resource),
 )
