664f5ef1fe6be0f6febacfd1526ebec2aa79877b
= <mogmi95@gmail.com>
2012-11-22 21:04:35 +0100

    Modified the name of the config dir to avoid problems while deployment on the server.

---
+++ hmm/urls.py
@@ -0,0 +1,12 @@
+from django.conf.urls import patterns, include, url
+
+# Uncomment the next two lines to enable the admin:
+from django.contrib import admin
+admin.autodiscover()
+
+urlpatterns = patterns('',
+    url(r'^admin/', include(admin.site.urls)),
+    (r'^grappelli/', include('grappelli.urls')),
+    (r'^api/strip/', include('strip.urls')),
+    (r'^api/bonus/', include('bonus.urls')),
+)
