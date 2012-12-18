78ec7782083397501fdb121bd805a98b4a081319
Mogmi <mogmi95@gmail.com>
2012-12-18 23:29:56 +0100

    Added static dir toi gitignore and deleted useless admin URL

--- hmm/urls.py
+++ hmm/urls.py
@@ -1,3 +1,4 @@
+from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 from django.conf.urls import patterns, include, url
 
 # Uncomment the next two lines to enable the admin:
@@ -5,10 +6,11 @@ from django.contrib import admin
 admin.autodiscover()
 
 urlpatterns = patterns('',
-    url(r'^admin/', include(admin.site.urls)),
     # Other
     url(r'^admin/', include(admin.site.urls)),
     (r'^grappelli/', include('grappelli.urls')),
     (r'^api/strip/', include('strip.urls')),
     (r'^api/bonus/', include('bonus.urls')),
 )
+
+urlpatterns += staticfiles_urlpatterns()
