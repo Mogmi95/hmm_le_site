e22aeaf9179358b665d0db29a37f4f73bac62dc2
= <mogmi95@gmail.com>
2013-01-07 18:50:38 +0100

    Added rss feed for strips

---
+++ strip/rss.py
@@ -0,0 +1,21 @@
+from django.contrib.syndication.views import Feed
+from strip.models import Strip
+
+class LatestEntriesFeed(Feed):
+    title = "Hmm-le-rss"
+    link = "/rss/"
+    description = "Les mises a jour de hmm-la-bd."
+    item_author_name = 'Mickael <Mogmi> Bidon'
+
+
+    def items(self):
+        return Strip.objects.order_by('number')[:10].reverse()
+
+    def item_title(self, item):
+        return item.title
+
+    def item_description(self, item):
+        return item.description
+
+    def item_author_name(self):
+        return self.item_author_name
